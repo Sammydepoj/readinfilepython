9999cdf422b07e5ca61b9fbf16436bc9f26b31e6
Sammydepoj <97622819+Sammydepoj@users.noreply.github.com>
2022-05-27 03:44:53 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+
+def read_file_content():
+    # [assignment] Add your code here 
+    file=open('story.txt','r')
+    data=file.readlines(())
+    return data
+
+
+def count_words():
+    text = read_file_content("./story.txt")
+    # [assignment] Add your code here
+    result=len(text.split)
+
+    return {"as": 10, "would": 20}
